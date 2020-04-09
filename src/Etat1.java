6432f7e9d7499f33616b8abae58cdf2fe9ed3631
Mistguard <alantournabien@hotmail.fr>
2020-04-09 17:06:48 +1100

    Correction des Etats

--- src/Etat1.java
+++ src/Etat1.java
@@ -2,9 +2,9 @@ public class Etat1 implements IEtat {
     @Override
     public void evenement1(Automate a) {
         System.out.println("Je passe dans l'état 2");
-        a.changementEtat(new Etat2());
         X x = a.getControlee();
         x.action1();
+        a.changementEtat(new Etat2());
     }
 
     @Override
--- src/Etat2.java
+++ src/Etat2.java
@@ -7,8 +7,8 @@ public class Etat2 implements IEtat {
     @Override
     public void evenement2(Automate a) {
         System.out.println("Je passe dans l'état 1");
-        a.changementEtat(new Etat1());
         X x = a.getControlee();
         x.action2();
+        a.changementEtat(new Etat1());
     }
 }
