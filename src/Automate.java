9172ddbe8175009219207c519a6281787332dd36
Mistguard <alantournabien@hotmail.fr>
2020-04-09 16:15:21 +1100

    Première construction suivant le diagramme UML

---
+++ src/Automate.java
@@ -0,0 +1,21 @@
+
+public class Automate implements IEvenement {
+
+    public IEtat etatCourant;
+
+    public IEtat changement(IEtat etat){
+        IEtat etat1 = new Etat1();
+
+        return etat1;
+    }
+
+    @Override
+    public void evenement1() {
+
+    }
+
+    @Override
+    public void evenement2() {
+
+    }
+}
---
+++ src/Etat1.java
@@ -0,0 +1,11 @@
+public class Etat1 implements IEtat {
+    @Override
+    public void evenement1(Automate a) {
+
+    }
+
+    @Override
+    public void evenement2(Automate a) {
+
+    }
+}
